0c84c4c9e78b90de15e98c22a1078f81a1bb1d4a
David Slater <dslater@dslater-lnx.igb.illinois.edu>
2025-09-04 15:17:18 -0500

    changed include_once to require_once

--- html/edit_user.php
+++ html/edit_user.php
@@ -1,6 +1,6 @@
 <?php
-	include_once 'includes/main.inc.php';
-	include_once 'includes/session.inc.php';
+	require_once 'includes/main.inc.php';
+	require_once 'includes/session.inc.php';
 	
 	if ( !$login_user->is_admin() ){
 		exit;
